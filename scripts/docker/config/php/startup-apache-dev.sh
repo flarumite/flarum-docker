eca7eaf2feed511ddf63033064f9d07fa6e3828c
Ian Morland <ianm@giffgaff.co.uk>
2020-05-10 03:02:31 +0100

    Pre-build assets on startup

--- scripts/docker/config/php/startup-apache-dev.sh
+++ scripts/docker/config/php/startup-apache-dev.sh
@@ -8,8 +8,8 @@ cd /var/www && php flarum cache:clear
 # We will also need to add the running of migrations here
 cd /var/www && php flarum migrate
 
-# Symlink fonts
-# ln -sF /var/www/vendor/components/font-awesome/webfonts /var/www/public/assets/fonts
+# Pre-compile assets
+cd /var/www && php flarum cache:assets --css --js --locales
 
 # Make the environment variables available for cron
 printenv | grep -v "no_proxy" >> /etc/environment
